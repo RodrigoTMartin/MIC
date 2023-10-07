0c5d75ab4d82e7bc13ed5792ec677514e5334047
RodrigoTMartin <martin.rodrigot@gmail.com>
2023-10-06 21:19:42 -0300

    Ejercicio 1 y 2 hechos

--- TP2/Input/cakeeating.jl
+++ TP2/Input/cakeeating.jl
@@ -170,3 +170,181 @@ function vfi!(ce::CakeEating; tol = 1e-8, maxiter = 2000, verbose = true)
 end
 
 print(" ✓\nConstructor: CakeEating(;β=0.96, r=0.02, γ=2, Nk = 20)\nSolver: vfi!(ce::CakeEating)\n")
+
+
+#Ejercicio 1 
+
+
+
+# Crear un objeto CakeEating con parámetros por defecto
+ce_default = CakeEating()
+
+#1 Resolver el problema utilizando vfi!
+vfi!(ce_default)
+
+#Creo la función para el consumo
+c_solo = ce_default.gc
+# Crear un rango de valores de capital
+k_values = ce_default.kgrid
+
+# Calcular la función de consumo como función del capital (c/k)
+c_over_k = ce_default.gc ./ k_values
+
+# Calcular la función de ahorro como función del capital (k'/k)
+savs_prime_over_k = ce_default.gk ./ k_values
+
+#2 Crear un gráfico de la función de consumo c/k como función de k
+plot_consumo = plot(
+    scatter(x=k_values, y=c_solo),
+    Layout(xaxis_title="Capital (k)", yaxis_title="Consumo (c)", title="Función de Consumo c vs Capital")
+)
+
+#3 Crear un gráfico de la función de consumo c/k como función de k
+plot_c_over_k = plot(
+    scatter(x=k_values, y=c_over_k, name="Consumo / Capital (c/k)"),
+    Layout(xaxis_title="Capital (k)", yaxis_title="Consumo / Capital (c/k)", title="Función de Consumo c/k vs Capital")
+)
+#4 Crear un gráfico de la función de ahorro k'/k como función de k
+
+plot_c_over_k = plot(
+    scatter(x=k_values, y=savs_prime_over_k),
+    Layout(xaxis_title="Capital (k)", yaxis_title="Ahorro / Capital (k'/k)", title="Función de Ahorro k'/k vs Capital")
+)
+
+#Voy a hacerlo con interpolaciones
+
+#Voy a definir primero otro elemento ce así no me piso.
+ce_defaultitp = CakeEating()
+# Resulvo el problema usando vfi_itp!
+vfi_itp!(ce_defaultitp)
+
+# Crear un rango de valores de capital
+k_values_itp = ce_defaultitp.kgrid
+
+# Calcular la función de consumo como función del capital (c/k)
+c_over_k_itp = ce_defaultitp.gc ./ k_values_itp
+#Calculo la función solo del consumo 
+c_solo_itp = ce_defaultitp.gc
+
+# Calcular la función de ahorro como función del capital (k'/k)
+savs_prime_over_k_itp = ce_defaultitp.gk ./ k_values_itp
+
+#5 Crear un gráfico de la función de consumo c/k como función de k
+
+
+plot_c_over_k_itp = plot(
+	scatter(x=k_values_itp, y=c_over_k_itp, name="Consumo / Capital (c/k)"),
+	Layout(xaxis_title="Capital (k)", yaxis_title="Consumo / Capital (c/k)", title="Función de Consumo c/k vs Capital")
+)
+
+#6 Crear un gráfico de la función de ahorro k'/k como función de k
+
+plot_k_over_k_itp = plot(
+	scatter(x=k_values_itp, y=savs_prime_over_k_itp),
+	Layout(xaxis_title="Capital (k)", yaxis_title="Ahorro / Capital (k'/k)", title="Función de Ahorro k'/k vs Capital")
+)
+
+#7 Crear un gráfico de la función de consumo c como función de k
+c_plot_itp = plot(
+	scatter(x=k_values_itp, y=c_solo_itp),Layout(xaxis_title="Capital (k)", yaxis_title="Consumo (c)", title="Función de Consumo c vs Capital")
+)
+
+
+
+
+### Ejercicio 2 - Simulador de torta
+
+#1. Establezco un tiempo máximo y un capital inicial
+
+T= 100
+k0 = 1
+# 2 .Inicio los vectores para guardar las sucesiones de consumo y capital
+
+C=zeros(T+1)
+K=zeros(T+1)
+
+# Establecer el estado inicial
+C[1] = k0
+K[1] = k0
+
+# 3. Crear interpoladores para las funciones de consumo y ahorro
+
+interpolate_consumo(k) = interpolate(ce_default.kgrid, ce_default.gc, k, Gridded(Linear()))
+
+interpolate_ahorro(k) = interpolate(ce_default.kgrid, ce_default.gk, k, Gridded(Linear()))
+
+# Simular el comportamiento a lo largo del tiempo
+# Simular el comportamiento a lo largo del tiempo
+for t in 1:T
+    k = K[t]
+    
+    # Usar el interpolador para obtener el consumo en el estado actual
+    C[t] = interpolate(interpolate_consumo, k)
+    
+    # Usar el interpolador para obtener el capital en el próximo período
+    K[t+1] = interpolate(interpolate_ahorro, k)
+end
+
+# Crear un gráfico del consumo a lo largo del tiempo
+plot_consumo = plot(0:T, C, xlabel="Tiempo", ylabel="Consumo", legend=false, title="Consumo a lo largo del tiempo")
+
+# Crear un gráfico de la torta que va quedando
+torta_restante = ce_default.kgrid[1] .- K
+plot_torta = plot(0:T, torta_restante, xlabel="Tiempo", ylabel="Torta Restante", legend=false, title="Torta Restante a lo largo del tiempo")
+
+# Mostrar los gráficos
+plot(plot_consumo, plot_torta, layout=(2, 1), size=(800, 600))
+
+
+
+#Segundo ejemplo: 
+
+# Definir tiempo máximo T y estado inicial k0
+T = 50
+k0 = 1
+
+# Inicializar vectores para guardar las sucesiones de consumo y capital
+C = zeros(T+1)
+K = zeros(T+1)
+
+# Establecer el estado inicial
+C[1] = k0
+K[1] = k0
+
+# Crear interpoladores para las funciones de consumo y ahorro
+interpolator_consumo = LinearInterpolation(ce_default.kgrid, ce_default.gc)
+interpolator_ahorro = LinearInterpolation(ce_default.kgrid, ce_default.gk)
+
+# Simular el comportamiento a lo largo del tiempo
+for t in 1:T
+    k = K[t]
+    
+    # Usar el interpolador para obtener el consumo en el estado actual
+    C[t] = interpolator_consumo(k)
+    
+    # Usar el interpolador para obtener el capital en el próximo período
+    K[t+1] = interpolator_ahorro(k)
+end
+
+# Crear un gráfico del consumo a lo largo del tiempo
+plot_consumo = plot(0:T, C, xlabel="Tiempo", ylabel="Consumo", legend=false, title="Consumo a lo largo del tiempo")
+
+# Crear un gráfico de la torta que va quedando
+torta_restante = ce_default.kgrid[1] .- K
+plot_torta = plot(0:T, torta_restante, xlabel="Tiempo", ylabel="Torta Restante", legend=false, title="Torta Restante a lo largo del tiempo")
+
+# Mostrar los gráficos
+plot(plot_consumo, plot_torta, layout=(2, 1), size=(800, 600))
+
+
+# Calcular el flujo acumulado de consumo
+flujo_acumulado_consumo = cumsum(C)
+
+# Crear un gráfico del flujo acumulado de consumo y la torta que va quedando
+plot(0:T, flujo_acumulado_consumo, xlabel="Tiempo", ylabel="Flujo Acumulado de Consumo",
+    label="Consumo Acumulado", legend=:topright, title="Flujo Acumulado de Consumo y Torta Restante")
+
+
+
+
+
